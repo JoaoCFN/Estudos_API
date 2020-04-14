eef44fd937adab6849d4dd65d1c334410125c72a
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-14 18:11:50 -0300

    Atualização nos métodos get, post e put

--- server.js
+++ server.js
@@ -2,7 +2,116 @@
 const express = require("express");
 // instâncio o o express na var app
 const app = express();
+
+// chamo os dados do data JSON
+const data = require("./data.json");
+
+// usar a notação JSON
+app.use(express.json());
+
+// VERBOS HTTP
+
+// GET: Recebe os dados de um resource
+// pega todos os clientes
+app.get("/clientes", function(req, res){
+    // resposta em formato json
+    res.json(data);
+});
+// pega um cliente específico
+app.get("/clientes/:id", function(req, res){
+    // pego o id do cliente do parâmetro com o req.params
+    const { id } = req.params;
+    // ele busca no JSON com o método find o id do cliente
+    const cliente = data.find(client => client.id == id);
+    
+    // se o clente não existir precisamos retorna algum erro legível para quem está consumindo nossa API
+    if (!cliente){
+        return res.status(204).json();
+    }
+    // responde um JSON com essa var
+    res.json(cliente);
+});
+
+// POST: Envia dados ou informações para serem processados por um resource
+// adiciona um novo cliente
+app.post("/clientes", function(req, res){
+    // pega nome e email
+    const {nome, email, idade} = req.body;
+    // cria o objeto novo cliente
+    const novo_cliente = {  
+        id: data.length + 1,
+        nome: nome,
+        email: email,
+        idade: idade    
+    }
+    // adiciona no JSON data
+    data.push(novo_cliente);
+    // mostra o resultado
+    res.json(data);
+});
+
+// PUT: Atualiza os dados de um resource
+// atualiza os dados de um cliente específico
+app.put("/clientes/:id", function(req, res){
+    // pego o id do cliente do parâmetro com o req.params
+    const { id } = req.params;
+    // ele busca no JSON com o método find o id do cliente
+    const cliente = data.find(client => client.id == id);
+
+    if(!cliente){
+        return res.status(204).json();
+    }
+
+    // pega o nome e o email
+    const { nome, email, idade } = req.body;
+    cliente.nome = nome;
+    cliente.email = email;
+    cliente.idade = idade
+
+    // responde
+    res.json(cliente);
+
+});
+
+// DELETE: Deleta um resource
+// deleta um cliente existente
+app.delete("/clientes/:id", function(req, res){
+    // pego o id do cliente do parâmetro com o req.params
+    const { id } = req.params;
+
+    const cliente = data.find(client => client.id == id);
+
+    if(!cliente){
+        return res.status(204).json();
+    }
+
+    // data.forEach((element, i) => {
+    //     if(cliente.id === id){
+    //         data.splice(i, 1)
+    //     }
+    // })
+
+    data.splice(id+1, 1);
+    res.json(data);
+    
+});
+
+
 // inicio o server na porta 3000 e recebo uma função de callback
 app.listen(3000, function(){
-    console.log("O servidor iniciou")
+    console.log("O servidor iniciou");
 });
+
+// TIPOS DE RESPOSTAS API
+
+// 1xx: Informação
+// 2xx: Sucesso
+//     200: OK
+//     201: CREATED
+//     204: Não tem conteúdo PUT POST DELETE
+// 3xx: Redirection
+// 4xx: Client Error
+//     400: Bad Request
+//     404: Not Found!
+// 5xx: Server Error 500: Internal Server Error
+// 500: Internal Server Error
